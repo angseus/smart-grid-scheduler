fb8dc70e3582f0a9000ceb3a8a65299566a85193
Victor Nordh <noordhan@gmail.com>
2016-09-15 09:18:02 +0200

    Updated the file with all basic nodes variables

--- node.py
+++ node.py
@@ -1,2 +1,11 @@
 # Should include
 #	- Simulating data for a specific node
+
+# Should have the following variables:
+# ID/Name
+# Power - Watt
+# Time - If battery charge or runtime to heat up the house, how long time to load?
+# Flexible - Can you schedule it? String with “on”,”flex” or “com”(case of emergency). If not flexible, maybe unnecessary in case of approaching a peak, switch off only then, like outside-lights or similar
+# Category
+# Priority - How important it is compared to other products
+# Group ID (Fas) - Feature to be able to restrict total consumption at the same phase
