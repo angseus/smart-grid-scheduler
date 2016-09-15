7db73709be2d1cde32e4270e5a3fc21eaddc882a
Johan Angséus <angseus@student.chalmers.se>
2016-09-15 10:44:15 +0200

    Sockets implemented for registration of nodes

--- node.py
+++ node.py
@@ -6,7 +6,44 @@
 # ID/Name
 # Power - Watt
 # Time - If battery charge or runtime to heat up the house, how long time to load?
-# Flexible - Can you schedule it? String with “on”,”flex” or “com”(case of emergency). If not flexible, maybe unnecessary in case of approaching a peak, switch off only then, like outside-lights or similar
+# Flexible - Can you schedule it? 
+# ... 0 means interactive loads since we cannot control them at all
+# ... 1 means background load that need to be run in a certain interval (slack), 
+# ... 2 (extended) means that we have a deadline, but nothing else to consider
 # Category
 # Priority - How important it is compared to other products
 # Group ID (Fas) - Feature to be able to restrict total consumption at the same phase
+
+
+import socket
+import sys
+import json
+
+    
+
+class Node():
+	def __init__(self, id, power, time, flexible, category, priority, group_id):
+		# Connect to the Smart Meter
+		HOST, PORT = "localhost", 9999
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.sock.connect((HOST, PORT))
+
+		self.id = id
+		self.power = power
+		self.time = time
+		self.flexible = flexible
+		self.category = category
+		self.priority = priority
+		self.group_id = group_id
+
+		register_data = {"id":self.id, "power":self.power, "time":self.time, "flexible":self.flexible,
+			"category":self.category, "priority":self.priority, "group_id":self.group_id}
+
+		data = {"action": "register", "payload":register_data}
+		# Convert to JSON and send
+		data = json.dumps(data).encode('utf-8')
+		self.sock.sendall(data)
+
+
+if __name__ == "__main__":
+	node = Node(0, 400, 0, 0, 0, 0, 1)
--- smart_meter.py
+++ smart_meter.py
@@ -1,8 +1,57 @@
 # Should include
-# 	- Algorithm
-# 	- Communication between all nodes
-#  	- Polling price from NordPool
+#   - Algorithm
+#   - Communication between all nodes
+#   - Polling price from NordPool
 
 # May have this structures:
-# 	- WaitingList = {id, time, power, (group id), deadline}
-# 	- Price = {Hour, avg price}
+#   - WaitingList = {id, time, power, (group id), deadline}
+#   - Price = {Hour, avg price}
+
+
+import socketserver
+import json
+
+node_list = []
+waiting_list = []
+active_list = []
+
+class RequestHandler(socketserver.BaseRequestHandler):
+    """
+    The request handler for incoming packages to the server.
+    """
+
+    def handle(self):
+        self.data = json.loads(self.request.recv(1024).decode('utf-8'))
+
+        """
+        Depending on the action, perform the proper operation.
+        """
+
+        action = self.data["action"]
+        payload = self.data["payload"]
+
+        # Register action
+        if (action == 'register'):
+            self.handle_register(payload)
+
+        # Request action
+        elif (action == 'request'):
+            self.handle_request(payload)
+
+        # Invalid, drop it 
+        else:
+            print('Invalid action received')
+
+    def handle_register(self, payload):
+        node_list.append(payload)
+        print (node_list)
+
+class SmartMeter():
+    def __init__(self):
+        # Server data
+        HOST, PORT = "localhost", 9999
+        self.server = socketserver.TCPServer((HOST, PORT), RequestHandler)
+        self.server.serve_forever()
+
+if __name__ == "__main__":
+    smart_meter = SmartMeter()
